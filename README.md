7c8ff1246cc9c24307f745c47b9c7b4c6933c4b8
cbdyzj <cbdyzj@jianzhao.org>
2021-08-13 08:34:25 +0800

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# x-user-js
+
+- [zhihu_simplify.user.js](./zhihu_simplify/zhihu_simplify.user.js)
---
+++ zhihu_simplify/zhihu_simplify.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name                zhihu_simplify
+// @description         zhihu simplify
+// @version             0.0.1
+// @namespace           https://github.com/alchemy-works
+// @author              Alchemy Works
+// @include             /^https:\/\/(www)\.zhihu\.com\/.*$/
+// @icon                https://www.google.com/s2/favicons?domain=zhihu.com
+// @license             MIT
+// @run-at              document-end
+// @grant               none
+// ==/UserScript==
+
+; (function () {
+    'use strict'
+    console.log('zhihu_simplify')
+})();
