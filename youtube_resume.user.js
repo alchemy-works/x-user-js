d2ede4d1fa5733cfcc8d33474cf7c4463009e276
Cbdy <cbdyzj@jianzhao.org>
2021-12-18 12:31:08 +0800

    Create  youtube_resume.user.js

---
+++ youtube_resume.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name                youtube_resume
+// @description         youtube_resume
+// @version             0.0.1
+// @namespace           https://github.com/alchemy-works
+// @author              Alchemy Works
+// @include             /^https:\/\/(www)\.youtube\.com\/.*$/
+// @icon                https://www.google.com/s2/favicons?domain=youtube.com
+// @license             MIT
+// @run-at              document-end
+// @grant               none
+// ==/UserScript==
+
+;(function () {
+    'use strict'
+
+})();
