e228cadc9eea0f77b9cd06df714c990b37961173
cbdyzj <cbdyzj@jianzhao.org>
2021-12-18 13:03:48 +0800

    Update youtube_resume.user.js

--- youtube_resume.user.js
+++ youtube_resume.user.js
@@ -5,6 +5,7 @@
 // @namespace           https://github.com/alchemy-works
 // @author              Alchemy Works
 // @include             /^https:\/\/(www)\.youtube\.com\/.*$/
+// @require             https://raw.githubusercontent.com/lawfx/YoutubeNonStop/v0.9.1/autoconfirm.js
 // @icon                https://www.google.com/s2/favicons?domain=youtube.com
 // @license             MIT
 // @run-at              document-end
@@ -13,21 +14,8 @@
 
 ;(function () {
     'use strict'
-
-    function resume() {
-        const resumeTextNode = document.querySelector('')
-        if (!resumeTextNode) {
-            return
-        }
-        const resumeConfirmButton = document.querySelector('')
-        if (!resumeConfirmButton) {
-            return
-        }
-        console.info(`[${new Date().toISOString()}]`, 'Resume playback')
-        resumeConfirmButton.click()
-    }
-
-    setInterval(() => {
-        resume()
-    }, 3000)
+    console.debug(
+        new Date().toISOString(),
+        'https://raw.githubusercontent.com/lawfx/YoutubeNonStop/v0.9.1/autoconfirm.js',
+    )
 })();
