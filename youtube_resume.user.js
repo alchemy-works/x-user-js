559078b0fb39b867135a1441ce7cde1d2d2faf33
cbdyzj <cbdyzj@jianzhao.org>
2021-12-18 16:16:30 +0800

    Amend

--- youtube_resume.user.js
+++ youtube_resume.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                youtube_resume
 // @description         youtube_resume
-// @version             0.0.3
+// @version             0.0.4
 // @namespace           https://github.com/alchemy-works
 // @author              Alchemy Works
 // @include             /^https:\/\/(www)\.youtube\.com\/.*$/
@@ -16,6 +16,7 @@
     const $ = document.querySelector.bind(document)
 
     function resume() {
+        const popupContainer = $('ytd-popup-container')
         const resumeTextNode = $('ytd-popup-container yt-formatted-string.line-text')
         if (!resumeTextNode || resumeTextNode.textContent !== '视频已暂停。是否继续观看？') {
             return
@@ -25,6 +26,11 @@
             return
         }
         resumeConfirmButton.click()
+        setTimeout(() => {
+            if (popupContainer) {
+                popupContainer.innerHTML = ''
+            }
+        })
         console.info(`[${new Date().toISOString()}]`, 'Resume playback')
     }
 
